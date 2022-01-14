8630c1e3601802508092d2d7e3a600efc89ff3c5
Corentin <corentincolnat@gmail.com>
2022-01-14 16:08:32 +0100

    mise en place icones

--- app.js
+++ app.js
@@ -44,25 +44,37 @@ function infoParis(){
         $("#temperature").html(Math.round(data["main"]["temp"]) + [" °C"]);
         $("#humidite").html(data["main"]["humidity"] + [" %"]);
         $("#vent").html(Math.round(data["wind"]["speed"]) + [ " km/h"]);
+        $('#nom').hide().fadeToggle(2000);              // hide permet d'afficher le contenu en cacher 
+        $('#temperature').hide().fadeToggle(2000);
+        $('#humidite').hide().fadeToggle(2000);
+        $('#vent').hide().fadeToggle(2000);
     });
 }
 
 function infoMetz(){
     $.getJSON('https://api.openweathermap.org/data/2.5/weather?q=metz&appid=cb5ce2ce842d9fe8f624dca7e7d327c5&units=metric', function(data) {
         console.log(data);
-    $("#nom").html(data["name"]);
-    $("#temperature").html(Math.round(data["main"]["temp"]) + [" °C"]);
-    $("#humidite").html(data["main"]["humidity"] + [" %"]);
-    $("#vent").html(Math.round(data["wind"]["speed"]) + [ " km/h"]);
+        $("#nom").html(data["name"]);
+        $("#temperature").html(Math.round(data["main"]["temp"]) + [" °C"]);
+        $("#humidite").html(data["main"]["humidity"] + [" %"]);
+        $("#vent").html(Math.round(data["wind"]["speed"]) + [ " km/h"]);
+        $('#nom').hide().fadeToggle(2000);
+        $('#temperature').hide().fadeToggle(2000);
+        $('#humidite').hide().fadeToggle(2000);
+        $('#vent').hide().fadeToggle(2000);
     });
 }
 
 function infoIstanbul(){
     $.getJSON('https://api.openweathermap.org/data/2.5/weather?q=istanbul&appid=cb5ce2ce842d9fe8f624dca7e7d327c5&units=metric', function(data) {
         console.log(data);
-    $("#nom").html(data["name"]);
-    $("#temperature").html(Math.round(data["main"]["temp"]) + [" °C"]);
-    $("#humidite").html(data["main"]["humidity"] + [" %"]);
-    $("#vent").html(Math.round(data["wind"]["speed"]) + [ " km/h"]);
+        $("#nom").html(data["name"]);
+        $("#temperature").html(Math.round(data["main"]["temp"]) + [" °C"]);
+        $("#humidite").html(data["main"]["humidity"] + [" %"]);
+        $("#vent").html(Math.round(data["wind"]["speed"]) + [ " km/h"]);
+        $('#nom').hide().fadeToggle(2000);
+        $('#temperature').hide().fadeToggle(2000);
+        $('#humidite').hide().fadeToggle(2000);
+        $('#vent').hide().fadeToggle(2000);
     });
 }
